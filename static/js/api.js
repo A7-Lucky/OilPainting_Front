bf4c5894a97302db2282f1ee605fc2e747a6c791
choding91 <whdlsrjf91@naver.com>
2022-11-25 23:13:59 +0900

    feat : 비밀번호 초기화 기능 추가 - 비밀번호 분실 시 본인 이메일로 인증 토큰을 받아 새로운 비밀번호 적용 가능하도록 구현
    Close #6

--- static/js/api.js
+++ static/js/api.js
@@ -234,3 +234,37 @@ async function updatePassword(formdata) {
     alert("비밀번호를 확인하세요!");
   }
 }
+
+// 비밀번호 분실(인증 토큰 발급)
+async function restPassword(formdata) {
+  const response = await fetch(`${backend_base_url}/users/api/password_reset/`, {
+    method: "POST",
+    body: formdata,
+  });
+
+  if (response.status == 200) {
+    alert("이메일 전송 완료!");
+    window.location.replace(`${frontend_base_url}/login.html`);
+  } else {
+    alert("이메일 주소를 확인하세요!");
+  }
+}
+
+// 비밀번호 등록(인증 토큰 입력)
+async function updateTokenPassword(formdata) {
+  let passwdCheck = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{8,16}$/);
+  const response = await fetch(`${backend_base_url}/users/api/password_reset/confirm/`, {
+    method: "POST",
+    body: formdata,
+  });
+
+  if (!passwdCheck.test(token_password.value)) {
+    alert("새 비밀번호는 영문 대문자와 소문자, 숫자, 특수문자를 하나 이상 포함하여 8~16자로 입력해주세요!");
+  } else if (response.status == 200) {
+    alert("비밀번호 변경 완료!");
+
+    window.location.replace(`${frontend_base_url}/login.html`);
+  } else {
+    alert("인증 토큰 및 비밀번호를 확인하세요!");
+  }
+}
--- static/js/login.js
+++ static/js/login.js
@@ -1,42 +1,25 @@
 console.log("login 로딩 완료");
 
-// window.onload = () => {
-//   console.log("로딩됨");
-// };
+// 이메일 주소 받기
+async function loadEmailAddress() {
+  const email_address = document.getElementById("email_address").value;
 
-// let username = document.querySelector("#username");
-// let password = document.querySelector("#password");
-// let btn = document.querySelector("#LoginBtn");
+  const formdata = new FormData();
 
-// btn.addEventListener("click", () => {
-//   // 아이디 정규식 (4~12자의 영문 대소문자와 숫자)
-//   var userCheck = RegExp(/^[a-zA-Z0-9]{4,12}$/);
-//   // 패스워드 정규식 (영문 대문자와 소문자, 숫자, 특수문자를 하나 이상 포함하여 8~16자)
-//   var passwdCheck = RegExp(
-//     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{8,16}$/
-//   );
+  formdata.append("email", email_address);
 
-//   // 아이디 공백 및 유효성 검사
-//   if (username.value == "") {
-//     alert("아이디를 입력해주세요!");
-//     username.focus();
-//     return false;
-//   } else if (!userCheck.test(username.value)) {
-//     alert("아이디는 4~12자의 영문 대소문자와 숫자로만 입력해주세요!");
-//     username.focus();
-//     return false;
-//   }
+  restPassword(formdata);
+}
 
-//   // 비밀번호 공백 및 유효성 검사
-//   if (password.value == "") {
-//     alert("비밀번호를 입력해주세요!");
-//     password.focus();
-//     return false;
-//   } else if (!passwdCheck.test(password.value)) {
-//     alert(
-//       "비밀번호는 영문 대문자와 소문자, 숫자, 특수문자를 하나 이상 포함하여 8~16자로 입력해주세요!"
-//     );
-//     password.focus();
-//     return false;
-//   }
-// });
+// 인증 토큰 및 비밀번호 받기
+async function loadTokenPassword() {
+  const token = document.getElementById("token").value;
+  const token_password = document.getElementById("token_password").value;
+
+  const formdata = new FormData();
+
+  formdata.append("token", token);
+  formdata.append("password", token_password);
+
+  updateTokenPassword(formdata);
+}
