562b1e08655741c3b8cf2034e8ff1ea1f5f78dcd
choding91 <whdlsrjf91@naver.com>
2022-11-27 14:57:23 +0900

    refact : 불필요 코드 정리

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,22 +1,5 @@
 console.log("profile 로딩 완료");
 
-// 로그인한 사용자 액세스 정보 추적 //
-function parseJwt(token) {
-  var base64Url = localStorage.getItem("access").split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
 // 프로필 보여주기
 window.onload = async function loadMyProfile() {
   profile = await getMyProfile();
