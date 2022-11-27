fc38b181bf79085f2c7bd3a13b90140d1e7d46b6
didjsrne <hyunjae0723@gmail.com>
2022-11-27 14:21:33 +0900

    fix : 기능추가
    - 아티클 get 주소 viewset으로 변경
    - 페이지네이션바 및 함수 추가
    - 아티클 create 모달 - 유화 스타일 추가

---
+++ static/js/api.js
@@ -0,0 +1,250 @@
+// 전역 변수 //
+const backend_base_url = "http://127.0.0.1:8000";
+const frontend_base_url = "http://127.0.0.1:5500/templates";
+
+var token = localStorage.getItem("access");
+
+// 주소로 아티클 페이지받기 //
+const urlParams = new URLSearchParams(window.location.search);
+const page_id = urlParams.get("page");
+
+
+// 로그인 //
+async function handleLogin() {
+    const loginData = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+    };
+    const response = await fetch(`${backend_base_url}/users/api/token/`, {
+        headers: {
+            Accept: "application/json",
+            "Content-type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(loginData),
+    });
+
+    response_json = await response.json();
+
+    if (response.status == 200) {
+        localStorage.setItem("access", response_json.access);
+        localStorage.setItem("refresh", response_json.refresh);
+
+        const base64Url = response_json.access.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split("")
+                .map(function (c) {
+                    return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+                })
+                .join("")
+        );
+
+        localStorage.setItem("payload", jsonPayload);
+        window.location.replace(`${frontend_base_url}/index.html`);
+    } else {
+        alert("로그인 정보를 확인하세요.", response.status);
+    }
+}
+
+
+// 로그아웃 //
+function handleLogout() {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    localStorage.removeItem("payload");
+
+    window.location.replace(`${frontend_base_url}/login.html`);
+}
+
+
+//index.html ------------------------------------------------------------------------->
+// 아티클 리스트 보여주기 //
+
+async function getArticleList() {
+    if (!page_id) {
+        var response = await fetch(`${backend_base_url}/articles/viewset/`, {
+            method: 'GET',
+        })
+    } else {
+        var response = await fetch(`${backend_base_url}/articles/viewset/?page=${page_id}`, {
+            method: 'GET',
+        })
+    }
+    
+    response_json = await response.json()
+    return response_json
+}
+
+
+// 아티클 디테일 페이지 연결 //
+function ArticleDetail(article_id) {
+    const url = `${frontend_base_url}/article_detail.html?id=${article_id}`;
+    location.href = url;
+}
+
+
+// 아티클 작성하기 //
+async function loadCreateArticle(title, content, image, style) {
+    const formdata = new FormData();
+
+    formdata.append('title', title)
+    formdata.append('content', content)
+    formdata.append('input', image)
+    formdata.append('style', style)
+
+    const response = await fetch(`${backend_base_url}/articles/`, {
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("access"),
+        },
+        method: 'POST',
+        body: formdata
+    })
+    if (response.status == 200) {
+        alert("글 작성 완료!")
+        window.location.replace(`${frontend_base_url}/index.html`)
+    }
+}
+
+
+//article_detail.html ------------------------------------------------------------------------->
+// 아티클 디테일 //
+async function GetArticle(article_id) {
+    const response = await fetch(`${backend_base_url}/articles/${article_id}`, {
+        method: "GET",
+    });
+    response_json = await response.json();
+    return response_json;
+}
+
+
+// 아티클 유저 프로필 가져오기 //
+async function GetProfile(article_id) {
+    const response = await fetch(`${backend_base_url}/articles/${article_id}/user/`, {
+        method: "GET",
+    });
+    response_json = await response.json();
+    return response_json;
+}
+
+
+// 댓글 리스트 가져오기 //
+async function GetComment(article_id) {
+    const response = await fetch(
+        `${backend_base_url}/articles/${article_id}/comment/`,
+        {
+            method: "GET",
+        }
+    );
+    response_json = await response.json();
+    return response_json;
+}
+
+
+// 댓글 작성하기 //
+async function loadCreateComment(comment) {
+    const response = await fetch(
+        `${backend_base_url}/articles/${article_id}/comment/`,
+        {
+            headers: {
+                "content-type": "application/json",
+                Authorization: "Bearer " + localStorage.getItem("access"),
+            },
+            method: "POST",
+            body: JSON.stringify({
+                article: article_id,
+                comment: comment,
+            }),
+        }
+    );
+
+    response_json = await response.json();
+    console.log(response_json);
+
+    if (response.status == 200) {
+        window.location.replace(
+            `${frontend_base_url}/article_detail.html?id=${article_id}`
+        );
+    } else {
+        alert(response.status);
+    }
+}
+
+
+// 댓글 수정하기 //
+async function loadUpdateComment(comment_id) {
+    const input_comment = document.getElementById("modal_comment").value;
+
+    const response = await fetch(
+        `${backend_base_url}/articles/${article_id}/comment/${comment_id}/`,
+        {
+            headers: {
+                "content-type": "application/json",
+                Authorization: "Bearer " + localStorage.getItem("access"),
+            },
+            method: "PUT",
+            body: JSON.stringify({
+                article: article_id,
+                comment: input_comment,
+            }),
+        }
+    );
+    response_json = await response.json();
+    console.log(response_json);
+
+    if (response.status == 200) {
+        window.location.replace(
+            `${frontend_base_url}/article_detail.html?id=${article_id}`
+        );
+    } else {
+        alert(response.status);
+    }
+}
+
+// 댓글 삭제하기 //
+async function loadDeleteComment(comment_id) {
+    const response = await fetch(
+        `${backend_base_url}/articles/${article_id}/comment/${comment_id}/`,
+        {
+            headers: {
+                "content-type": "application/json",
+                Authorization: "Bearer " + localStorage.getItem("access"),
+            },
+            method: "DELETE",
+        }
+    );
+
+    if (response.status == 204) {
+        window.location.replace(
+            `${frontend_base_url}/article_detail.html?id=${article_id}`
+        );
+    } else {
+        alert(response.status);
+    }
+}
+
+
+// 유저 정보 가져오기 --------------------------------------------------------------------------->
+// 로그인한 유저 가져오기 //
+async function getName() {
+    const response = await fetch(`${backend_base_url}/users/profile`, {
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("access"),
+        },
+        method: "GET",
+    });
+
+    if (response.status == 200) {
+        response_json = await response.json();
+        return response_json;
+    } else {
+        return null;
+    }
+}
+
+
+
+function darkmode() {
+    document.getElementById('body').classList.toggle('dark');
+  }
---
+++ static/js/index.js
@@ -0,0 +1,83 @@
+// 로그인한 사용자 액세스 정보 추적 //
+function parseJwt(token) {
+    var base64Url = localStorage.getItem("access").split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+  
+    return JSON.parse(jsonPayload);
+  }
+
+
+// 유저 정보 가져오기 //
+async function checkLogin(){
+    const name = await getName();
+    
+    const username = document.getElementById("username")
+    username.innerText = name.email + " 님 환영합니다!"
+}
+checkLogin()
+
+
+
+// 아티클 리스트 가져오기 (임시) //
+window.onload = async function loadArticleList(){
+    articles = await getArticleList()
+    console.log(articles)
+    articles = articles['results']
+    console.log(articles)
+    const article_list = document.getElementById("article_list")
+
+    articles.forEach(article => {
+        // 사진만 들어가는 버전
+        const newimage = document.createElement("img")
+
+        newimage.setAttribute("src", `${backend_base_url}/${article.image}`)
+        newimage.setAttribute("id", article.id)
+
+        newimage.setAttribute("onclick", "ArticleDetail(this.id)")
+
+        article_list.appendChild(newimage)
+
+        // 제목+내용까지
+        // const newuser = document.createElement("ol")
+        // const newtitle = document.createElement("ol")
+        // newtitle.setAttribute("id", article.id)
+        // newuser.innerText = article.user
+        // newtitle.innerText = article.title
+        // newtitle.setAttribute("onclick", "ArticleDetail(this.id)")
+        // article_list.appendChild(newuser)
+        // article_list.appendChild(newtitle)
+    });
+}
+
+
+// 아티클 생성 (임시) //
+async function CreateArticle() {
+    const title = document.getElementById("title").value
+    const content = document.getElementById("content").value
+    const image = document.getElementById("image").files[0]
+    const style = $('input[name=setstyle]:checked').val();
+    loadCreateArticle(title, content, image, style)
+}
+
+function MovePrevious() {
+    if (!page_id || page_id == 1) {
+      alert("첫 페이지입니다")
+    } else {
+      var newpage_id = Number(page_id) - 1
+      location.replace(`${frontend_base_url}/index.html?page=${newpage_id}`)
+    }
+}
+
+function MoveNext() {
+  var newpage_id = Number(page_id) + 1
+  location.replace(`${frontend_base_url}/index.html?page=${newpage_id}`)
+}
