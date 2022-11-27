4f58d830351f80c05f5aabe6f18271e2db0f0f88
jaewon <jaewoniik94@gmail.com>
2022-11-27 16:57:40 +0900

    docs : 불필요한 코드 정리

--- static/js/index.js
+++ static/js/index.js
@@ -39,3 +39,4 @@ async function CreateArticle() {
     const content = document.getElementById("content").value
     const image = document.getElementById("image").files[0]
     loadCreateArticle(title, content, image)
+}
